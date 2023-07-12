29213e3fdbbf455705c7345f8f3216ac5e6b1687
Agasthya Yerrapragada <yerrapragada.agasthya@gmail.com>
2023-07-12 15:39:10 -0400

    Moved files and removed folder

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div class='header'>
+      <h1>NBA Stats</h1>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <a class='active' href='#home'>
+            Home
+          </a>
+        </li>
+        <li>
+          <a href='#news'>News</a>
+        </li>
+        <li>
+          <a href='#today'>Today</a>
+        </li>
+        <li>
+          <a href='#about'>About</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
